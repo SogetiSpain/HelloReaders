f75f6e7f0a14719f21e354976a1a75b72825852d
cmendible <cmendible@outlook.com>
2016-05-16 09:53:56 +0200

    Subiendo las fuentes

---
+++ src/HelloReaders/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloReaders
+{
+	using System;
+	
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Readers!!! from Raspberry Pi");
+        }
+    }
+}
